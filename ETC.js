099aa6c3e6eb8b8dba14b323baf189311c327216
Eunomiac <ryan.o.west@gmail.com>
2021-06-28 02:00:08 -0400

    Image updates

--- ETC.js
+++ ETC.js
@@ -164,6 +164,59 @@ const EunosTextControls = (() => {
             const UPSHIFT = -26;   // Constants governing how the chat box is positioned in the chat panel: By default, everything
             const LEFTSHIFT = -45; // shifts up and to the left to cover the standard chat output with the custom styles below.
             const BOTTOMSHIFT = -7;
+            const ApplyStyles = () => {
+                // For each entry in HTML, find all elements with matching class. Extract "style" line, convert to object, submit as 'styles' parameter
+                const processStyles = (elem, HTMLref, isButton) => {
+                    const styleData = {};
+                    (elem.getAttribute("style") || "")
+                        .split(/;/u).filter((style) => Boolean(style))
+                        .map((styleString) => (styleString || "").split(/:/).map((prop) => prop.trim()))
+                        .forEach(([key, val]) => { styleData[key] = val } );
+                    const title = elem.title;
+                    let elemString;
+                    if (/^H\d$/.test(elem.tagName)) {
+                        elemString = HTMLref("**CONTENT**", parseInt(elem.tagName.slice(1)), styleData, title);
+                    } else if (Array.from(elem.classList).filter((cls) => cls.startsWith("Button")).length) {
+                        const [href, name] = elem.innerHTML.match(/href\s*=\s*\S(.*)\S.*>(.*)</u).slice(1);
+                        elemString = HTMLref(name, href, styleData, title);
+                        const [wrapperStyle, linkStyle] = elemString
+                            .match(/(?:style\s*=\s*\S([^"]*)\S)/gu)
+                            .map((styleString) => styleString.replace(/^.*=['"](.*)['"]$/gu, "$1"));
+                        elem.innerHTML = `<a href='${href}' style='${linkStyle}'>${name}</a>`;
+                        console.log([
+                            elemString,
+                            href, name,
+                            `Box Styles: ${wrapperStyle}`,
+                            `Link Styles: ${linkStyle}`
+                        ].join("\n"));
+                    } else if (elem.tagName === "A") {
+                        return;
+                        // elemString = HTMLref("**COMMAND**", styleData, title);
+                        // console.log(`Link Element: ${elemString}`);
+                    } else {
+                        elemString = HTMLref("**CONTENT**", styleData, title);
+                    }
+                    const combinedStyles = elemString.replace(/[a-zA-Z-]*: (;|$)/gu, "").trim().replace(/\s+/gu, " ").match(/style\s*=\s*\S([^"]*)\S/u)[1];
+                    if (elem.classList.contains("Button") || elem.tagName === "A") {
+                    }
+                    elem.style = combinedStyles;
+                };
+                Object.keys(HTML).forEach((classOrTag) => {
+                    if (classOrTag === "H") {
+                        ["H1", "H2", "H3", "H4", "H5"].forEach((header) => {
+                            const hLevel = parseInt(header.slice(1));
+                            Array.from(document.getElementsByTagName(header)).forEach((elem) => { processStyles(elem, HTML.H, hLevel) });
+                        });
+                    } else if (classOrTag === "Paras") {
+                        Array.from(document.getElementsByTagName("P")).forEach((elem) => { processStyles(elem, HTML.Paras) });
+                    } else if (classOrTag.startsWith("Button")) {
+                        Array.from(document.getElementsByClassName(classOrTag)).forEach((elem) => { processStyles(elem, HTML[classOrTag]), true});
+                        Array.from(document.getElementsByTagName("A")).forEach((elem) => { processStyles(elem, HTML.A) });
+                    } else {
+                        Array.from(document.getElementsByClassName(classOrTag)).forEach((elem) => { processStyles(elem, HTML[classOrTag]) });
+                    }
+                });
+            };
 
             const HTML = {
                 Box: (content, styles = {}, title = undefined) => `<div style="${U.Style(Object.assign({
@@ -172,19 +225,18 @@ const EunosTextControls = (() => {
                     "height": "auto", "min-height": "32px",
                     "margin": `${UPSHIFT}px 0 ${BOTTOMSHIFT}px ${LEFTSHIFT}px`,
                     "padding": "0",
-                    "color": "gold",
+                    "color": COLORS.palegold,
                     "text-align": "center",
                     "position": "relative",
-                    "text-shadow": "none", "box-shadow": "none",
-                    "background-image": `url('${GetImgURL("blackLeather_2.jpg", "texture")}')`,
-                    "border": "4px outset #666",
-                    "overflow": "hidden",
-                    "border-radius": "33px"
+                    "text-shadow": "none", "box-shadow": "none", "border": "none",
+                    "background-image": `url('${GetImgURL("BG.jpg", "general")}')`,
+                    "background-size": "100%",
+                    "overflow": "hidden"
                 }, styles))}"${title ? ` title="${title}"` : ""}>${[content].flat().join("")}</div>`,
                 Block: (content, styles = {}, title = undefined) => `<div style="${U.Style(Object.assign({
-                    "width": "97%",
+                    "width": `${CHATWIDTH - 16}px`,
                     "margin": "2px 0 0 0",
-                    "padding": "1.5%",
+                    "padding": "0 8px",
                     "text-align": "left",
                     "background": "none",
                     "font-family": "serif",
@@ -192,25 +244,27 @@ const EunosTextControls = (() => {
                     "font-size": "14px",
                     "line-height": "18px"
                 }, styles))}"${title ? ` title="${title}"` : ""}>${[content].flat().join("")}</div>`,
-                Header: (content, styles = {}, title = undefined) => `<span style="${U.Style(Object.assign({
+                Title: (content, styles = {}, title = undefined) => `<div style="${U.Style(Object.assign({
                     "display": "block",
-                    "height": "auto", "min-height": "32px",
+                    "height": "142px",
                     "width": "auto",
-                    "margin": "0",
-                    "padding": "0 2px",
-                    "text-align": "center",
-                    "color": "#ffd400",
-                    "font-family": "serif",
-                    "font-size": "18px",
-                    "line-height": "32px",
-                    "font-variant": "small-caps",
-                    "background-color": "transparent",
-                    "background-image": `url('${GetImgURL("blackLeather_1.jpg", "texture")}')`,
+                    "margin": "0 0 -30px 0",
+                    "background-image": `url('${GetImgURL("TOP.png", "general")}')`,
                     "background-size": "cover",
                     "font-weight": "normal",
-                    "border": "none", "box-shadow": "none",
-                    "text-shadow": "1px 1px 2px rgba(255, 255, 255, 0.8), -1px -1px 2px rgb(0, 0, 0), -1px -1px 2px rgb(0, 0, 0), -1px -1px 2px rgb(0, 0, 0)"
-                }, styles))}"${title ? ` title="${title}"` : ""}>${[content].flat().join("")}</span>`,
+                    "border": "none", "box-shadow": "none", "text-shadow": "none"
+                }, styles))}"${title ? ` title="${title}"` : ""}>${[content].flat().join("")}</div>`,
+                Footer: (content, styles = {}, title = undefined) => `<div style="${U.Style(Object.assign({
+                    "display": "block",
+                    "height": "37px",
+                    "width": "auto",
+                    "margin": "6px 0 0 0",
+                    "background-image": `url('${GetImgURL("BOTTOM.png", "general")}')`,
+                    "background-size": "100%",
+                    "background-repeat": "no-repeat",
+                    "font-weight": "normal",
+                    "border": "none", "box-shadow": "none", "text-shadow": "none"
+                }, styles))}"${title ? ` title="${title}"` : ""}>${[content].flat().join("")}</div>`,
                 CodeBlock: (content, styles = {}, title = undefined) => HTML.Block(content, Object.assign({
                     "background": "white",
                     "font-family": "monospace",
@@ -224,11 +278,9 @@ const EunosTextControls = (() => {
                     "font-size": "12px",
                     "color": "black",
                     "text-shadow": "0 0 1px black",
-                    "background-image": `url('${GetImgURL("gold_2.jpg", "texture")}')`,
+                    "background-image": `url('${GetImgURL("CodeSpanBG.png")}')`,
                     "padding": "0 7px 0 5px",
-                    "border-radius": "8px",
-                    "background-size": "cover",
-                    "border": "2px solid gold"
+                    "background-size": "100% 100%"
                 }, styles))}"${title ? ` title="${title}"` : ""}>${[content].flat().join("")}</span>`,
                 ButtonWide: (name, command, styles = {}, title = undefined) => `<span style="${U.Style(Object.assign({
                     "display": "inline-block",
@@ -237,36 +289,66 @@ const EunosTextControls = (() => {
                     "vertical-align": "baseline",
                     "margin": "0",
                     "float": "none"
-                }, _.pick(styles, "display", "width", "text-align", "vertical-align", "margin", "float")))}"${title ? ` title="${title}"` : ""}><a href="${command}" style="${U.Style(Object.assign({
-                    display: "inline-block",
-                    width: "90%",
-                    padding: "5px",
-                    "margin-top": "0",
-                    background: "gold",
-                    color: "black",
-                    "font-family": "sans-serif",
-                    "text-transform": "uppercase",
-                    "font-weight": "bold",
-                    "border-radius": "10px",
-                    border: "2px outset #666",
-                    "font-size": "14px",
-                    "line-height": "18px"
-                }, _.omit(styles, "display", "width", "text-align", "vertical-align", "margin", "float")))}">${name}</a></span>`,
+                }, _.pick(styles, "display", "width", "text-align", "vertical-align", "margin", "float")))}"${title ? ` title="${title}"` : ""}>${HTML.A(name, command, _.omit(styles, "display", "width", "text-align", "vertical-align", "margin", "float"), title)}</span>`,
+                ButtonRound: (imgRef, command, styles = {}, title = undefined) => {
+                    return `<span style="${U.Style(Object.assign({}, {
+                        "display": "inline-block",
+                        "width": "50px",
+                        "height": "50px",
+                        "background-image": `url('${GetImgURL(imgRef)}')`,
+                        "vertical-align": "baseline",
+                        "margin": "0",
+                        "float": "none"
+                    }, _.pick(styles, "display", "width", "background-image", "vertical-align", "margin", "float")))}"${title ? ` title="${title}"` : ""}>${HTML.A("", command, Object.assign({}, styles, {
+                        "width": "50px",
+                        "height": "50px",
+                        "padding": "0",
+                        "margin": "0"
+                    }), title)}</span>`;
+                },
+                A: (content, command, styles = {}, title = undefined) => {
+                    return `<a href="${command}" style="${U.Style(Object.assign({
+                        display: "inline-block",
+                        width: "90%",
+                        padding: "5px",
+                        "margin-top": "0",
+                        background: "gold",
+                        color: "black",
+                        "font-family": "sans-serif",
+                        "text-transform": "uppercase",
+                        "font-weight": "bold",
+                        "border-radius": "10px",
+                        border: "2px outset #666",
+                        "font-size": "14px",
+                        "line-height": "18px"
+                    }, styles))}">${[content].flat().join("")}</a>`;
+                },
                 H: (content, level = 3, styles = {}, title = undefined) => `<h${level} style="${U.Style(Object.assign([
                     null, // <H0>
                     { // <H1>
+                        display: "block",
+                        height: "43px", width: "285px",
+                        margin: "20px 0 -10px -9px",
+                        "font-family": "Impact",
+                        "line-height": "36px",
+                        "font-size": "24px",
+                        "font-weight": "normal",
+                        color: "black",
+                        "text-align": "center",
+                        "background-image": `url('${GetImgURL("H1.png")}')`,
+                        "background-size": "100% 100%"
                     },
                     { // <H2>
                         display: "block",
-                        height: "26px",
-                        "font-family": "sans-serif",
+                        height: "26px", width: "285px",
+                        "margin": "15px 0 -5px -9px",
+                        "font-family": "Trebuchet MS",
                         "line-height": "26px",
-                        "margin": "0 0 9px -2%",
-                        color: "gold",
-                        "text-indent": "4px",
-                        width: "105%",
-                        "background-image": `url('${GetImgURL("blackLeather_1.jpg", "texture")}')`,
-                        "border": "2px solid black",
+                        "font-size": "16px",
+                        color: "black",
+                        "text-indent": "10px",
+
+                        "background-image": `url('${GetImgURL("H2.png")}')`,
                         "background-size": "cover"
                     },
                     { // <H3>
@@ -299,9 +381,9 @@ const EunosTextControls = (() => {
                 ][level], styles))}"${title ? ` title="${title}"` : ""}>${[content].flat().join("")}</h${level}>`,
                 Spacer: (height, display = "block") => `<span style="${U.Style({display, height})}">&nbsp;</span>`,
                 Paras: (content, styles = {}) => [content].flat().map((para) => `<p style="${U.Style(Object.assign({
+                    "margin": "10px 0",
                     "line-height": "16px",
-                    "font-family": "sans-serif",
-                    "text-shadow": "1px 1px 2px rgba(255, 255, 255, 0.7), -2px -2px 3px black, -2px -2px 3px black, -2px -2px 3px black"
+                    "font-family": "sans-serif"
                 }, styles))}">${para}</p>`).join(""),
                 Span: (content, styles = {}, title = undefined) => `<span style="${U.Style(Object.assign({
                     "display": "inline-block",
@@ -313,7 +395,8 @@ const EunosTextControls = (() => {
                 }, styles))}"${title ? ` title="${title}"` : ""}>${[content].flat().join("")}</span>`, //  bgColor = "none", color = "black", fontSize = "14px", lineHeight = "18px") =>
                 Img: (imgSrc, styles = {}, title = undefined) => `<img src="${imgSrc}" style="${U.Style(styles)}"${title ? ` title="${title}"` : ""}>`
             };
-            const ApplyStyles = () => {
+
+            const ApplStyles = () => {
                 // For each entry in HTML, find all elements with matching class. Extract "style" line, convert to object, submit as 'styles' parameter
                 const processStyles = (elem, HTMLref) => {
                     console.log(elem, HTMLref);
@@ -971,7 +1054,7 @@ const EunosTextControls = (() => {
                         D.HTML.Img("https://raw.githubusercontent.com/Eunomiac/-EunosTextControls/master/images/Header%20-%20Text%20Shadows%200.1.jpg"),
                         D.HTML.Paras([
                             "Add plea&shy;sant sha&shy;dows to sand&shy;box text ob&shy;jects in Roll20 — either <b>auto&shy;matically</b>, when&shy;ever new text is ad&shy;ded to the sand&shy;box, or <b>man&shy;ually</b>, by se&shy;lect&shy;ing text ob&shy;jects and re&shy;gister&shy;ing them for a sha&shy;dow via the com&shy;mands be&shy;low.",
-                            "Sha&shy;dow ob&shy;jects are in&shy;tended to be hands off: They're crea&shy;ted auto&shy;ma&shy;ti&shy;cally when re&shy;gist&shy;ered, will up&shy;date when&shy;ever their mas&shy;ter text ob&shy;ject's po&shy;sition and/or con&shy;tent changes, and will be re&shy;moved if the mas&shy;ter ob&shy;ject is ever de&shy;leted."
+                            "Sha&shy;dow ob&shy;jects are in&shy;tended to be hands off: They're crea&shy;ted auto&shy;ma&shy;ti&shy;cally when re&shy;gist&shy;ered, will up&shy;date when&shy;ever their mas&shy;ter text ob&shy;ject's po&shy;sition and/or con&shy;tent chan&shy;ges, and will be re&shy;moved if the mas&shy;ter ob&shy;ject is ever de&shy;leted."
                         ]),
                         D.HTML.H("Chat Commands for Text Shadows", 4),
                         D.HTML.Paras([
